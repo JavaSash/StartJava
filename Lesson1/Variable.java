d5ce201a1afcb71769688d326b8adb3f62894c9e
Александр Степенко <alexandrstepenko@gmail.com>
2020-10-24 18:23:24 +0300

    Исправил форматирование MyFirstApp и Variables

--- Lesson1/Variable.java
+++ Lesson1/Variable.java
@@ -1,25 +1,22 @@
 public class Variable {
-
-    public static void main (String[] args){
-
+    public static void main(String[] args) {
         byte ram = 4;
-        short windows = 8;
+        short available = 3;
         int bitnessOfTheOS = 64;
         long productCode = 190700000000100L;
         float cpuFrequency = 1.5f;
         double performanceIndex = 4.8;
-        char copyright = 169;
+        char windows = 8;
         boolean notebook = true;
-
         System.out.println("Brief information about computer of JavaSash:");
         System.out.println("Notebook: " + notebook);
         System.out.println("RAM memory: " + ram + " Gb");
-        System.out.println("Version of Windows: " + windows);
+        System.out.println("Available RAM: " + available + " Gb");
+        System.out.print("Version of Windows: ");
+        System.out.println(windows);
         System.out.println("Type of System: " + bitnessOfTheOS + "-bit operating system");
         System.out.println("Product code: " + productCode);
         System.out.println("Processor: " + cpuFrequency + " GHz");
         System.out.println("Performance index of Windows: " + performanceIndex);
-        System.out.println(copyright + " Microsoft Corporation");
-
     }
 }
