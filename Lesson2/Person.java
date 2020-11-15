df71ad5fb2844bac38dc06e1bc5e77547e366e44
Александр Степенко <alexandrstepenko@gmail.com>
2020-11-15 23:55:33 +0300

    Fix Person

--- Lesson2/Person.java
+++ Lesson2/Person.java
@@ -4,24 +4,29 @@ public class Person {
     double height = 1.75;
     double weight = 70.4;
     int age = 37;
-    public go (int speed) {
+
+    public go(int speed) {
         if(speed > 0 && speed < 7) {
             System.out.println(name + " идёт со скоростью " + speed + " км/ч.");
-            }
+        }
         return speed;
     }
+
     public void sit() {
         System.out.println(name + " сидит.");
     }
+
     public run(int speed) {
         if(speed > 6) {
             System.out.println(name + " бежит со скоростью " + speed + " км/ч.");
         }
         return speed;
     }
+
     public void speak() {
         System.out.println(name + " говорит: \"Привет!\"");
     }
+
     public void learnJava() {
         System.out.println(name + " учит Java.");
     }
