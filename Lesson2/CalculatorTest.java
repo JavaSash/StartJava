5eb07e28e7811cd3c52317cea30a133c21909da2
Александр Степенко <alexandrstepenko@gmail.com>
2020-11-23 19:13:03 +0300

    Revert "Add working Calc & CalcTest"
    This reverts commit 7f1a2a3aee2d40dda04b6e5643ef97f86eab615d.

--- Lesson2/CalculatorTest.java
+++ Lesson2/CalculatorTest.java
@@ -2,28 +2,38 @@ import java.util.Scanner;
 
 public class CalculatorTest {
     public static void main(String[] args) {
+        //int numb1, numb2, result;
+        //char mathOperation;
 
-        Calculator calc = new Calculator();
+        Calculator numb1 = new Calculator();
         System.out.println("Введите число");
         Scanner scan1 = new Scanner(System.in);
         if(scan1.hasNextInt()) {
-            calc.setNumb1(scan1.nextInt());
+            numb1.setNumb1(scan1.nextInt());
         } else {
             System.out.println("Вы ввели не число");
         }
 
+        Calculator mathOperation = new Calculator();
         System.out.println("Введите математическую операцию");
         Scanner scanMathOperation = new Scanner(System.in);
-        calc.setMathOperation(scanMathOperation.next().charAt(0));
+        mathOperation.setMathOperation(scanMathOperation.next().charAt(0));
+        /*if(scanMathOperation.hasNextChar) {
+            mathOperation.setMathOperation(scanMathOperation.next().charAt(0));
+        } else {
+            System.out.println("Вы ввели не математический знак");
+        }*/
 
+        Calculator numb2 = new Calculator();
         System.out.println("Введите второе число");
         Scanner scan2 = new Scanner(System.in);
         if(scan2.hasNextInt()) {
-            calc.setNumb2(scan2.nextInt());
+            numb2.setNumb2(scan2.nextInt());
         } else {
             System.out.println("Вы ввели не число");
         }
 
-        calc.getResult();
+        Calculator result = new Calculator();
+        result.getResult();
     }
 }
--- Lesson2/JaegerTest.java
+++
@@ -1,5 +0,0 @@
-public class JaegerTest {
-    public static void main(String[] args) {
-        Jaeger jaegerOne = new Jaeger();
-    }
-}
