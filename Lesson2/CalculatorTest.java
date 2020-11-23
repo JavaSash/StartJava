7f1a2a3aee2d40dda04b6e5643ef97f86eab615d
Александр Степенко <alexandrstepenko@gmail.com>
2020-11-23 19:08:46 +0300

    Add working Calc & CalcTest

--- Lesson2/CalculatorTest.java
+++ Lesson2/CalculatorTest.java
@@ -2,38 +2,28 @@ import java.util.Scanner;
 
 public class CalculatorTest {
     public static void main(String[] args) {
-        //int numb1, numb2, result;
-        //char mathOperation;
 
-        Calculator numb1 = new Calculator();
+        Calculator calc = new Calculator();
         System.out.println("Введите число");
         Scanner scan1 = new Scanner(System.in);
         if(scan1.hasNextInt()) {
-            numb1.setNumb1(scan1.nextInt());
+            calc.setNumb1(scan1.nextInt());
         } else {
             System.out.println("Вы ввели не число");
         }
 
-        Calculator mathOperation = new Calculator();
         System.out.println("Введите математическую операцию");
         Scanner scanMathOperation = new Scanner(System.in);
-        mathOperation.setMathOperation(scanMathOperation.next().charAt(0));
-        /*if(scanMathOperation.hasNextChar) {
-            mathOperation.setMathOperation(scanMathOperation.next().charAt(0));
-        } else {
-            System.out.println("Вы ввели не математический знак");
-        }*/
+        calc.setMathOperation(scanMathOperation.next().charAt(0));
 
-        Calculator numb2 = new Calculator();
         System.out.println("Введите второе число");
         Scanner scan2 = new Scanner(System.in);
         if(scan2.hasNextInt()) {
-            numb2.setNumb2(scan2.nextInt());
+            calc.setNumb2(scan2.nextInt());
         } else {
             System.out.println("Вы ввели не число");
         }
 
-        Calculator result = new Calculator();
-        result.getResult();
+        calc.getResult();
     }
 }
