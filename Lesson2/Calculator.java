422a142b1334747e2f0d03dbdec076332ec2f4df
Александр Степенко <alexandrstepenko@gmail.com>
2020-11-25 21:53:31 +0300

    Add to Cacl & CalcTest continue yes/no feature

--- Lesson2/Calculator.java
+++ Lesson2/Calculator.java
@@ -1,10 +1,12 @@
 public class Calculator {
-    private int numb1, numb2;
+    private int numb1;
+    private int numb2;
+    private int result;
     private char mathOperation;
 
     public void setNumb1(int numb1) {
         this.numb1 = numb1;
-        }
+    }
 
     public void setMathOperation(char mathOperation) {
         this.mathOperation = mathOperation;
@@ -14,32 +16,26 @@ public class Calculator {
         this.numb2 = numb2;
     }
 
-    private int result;
-    public int getResult() {
+    public int calculate() {
         switch(mathOperation) {
             case '+':
                 result = numb1 + numb2;
-                System.out.println(result);
                 break;
             case '-':
                 result = numb1 - numb2;
-                System.out.println(result);
                 break;
             case '*':
                 result = numb1 * numb2;
-                System.out.println(result);
                 break;
             case '/':
                 if(numb2 != 0) {
                     result = numb1 / numb2;
-                    System.out.println(result);
                 } else {
                     System.out.println("На ноль делить нельзя");
                 }
                 break;
             case '%':
                 result = numb1 % numb2;
-                System.out.println(result);
                 break;
             case '^':
                 int degree = 1;
@@ -48,11 +44,11 @@ public class Calculator {
                     result *= numb1;
                     degree++;
                 }
-                System.out.println(result);
                 break;
             default:
                 System.out.println("Нет такой математической операции.");
         }
+        System.out.println(result);
         return result;
     }
 }
--- Lesson2/CalculatorTest.java
+++ Lesson2/CalculatorTest.java
@@ -2,34 +2,50 @@ import java.util.Scanner;
 
 public class CalculatorTest {
     public static void main(String[] args) {
-        String yesNo;
+        String answer;
+
         do {
+            boolean yesNo = true;
+
             Calculator calc = new Calculator();
             System.out.println("Введите число");
-            Scanner scan1 = new Scanner(System.in);
-            if(scan1.hasNextInt()) {
-                calc.setNumb1(scan1.nextInt());
+            Scanner scan = new Scanner(System.in);
+            if(scan.hasNextInt()) {
+                calc.setNumb1(scan.nextInt());
             } else {
                 System.out.println("Вы ввели не число");
             }
 
             System.out.println("Введите математическую операцию");
-            Scanner scanMathOperation = new Scanner(System.in);
-            calc.setMathOperation(scanMathOperation.next().charAt(0));
+            calc.setMathOperation(scan.next().charAt(0));
 
             System.out.println("Введите второе число");
-            Scanner scan2 = new Scanner(System.in);
-            if(scan2.hasNextInt()) {
-                calc.setNumb2(scan2.nextInt());
+            if(scan.hasNextInt()) {
+                calc.setNumb2(scan.nextInt());
             } else {
                 System.out.println("Вы ввели не число");
             }
 
-            calc.getResult();
+            calc.calculate();
 
-            System.out.println("Хотите продолжить? [y/n]");
+            System.out.println("Вы хотите продолжить? [y/n]");
             Scanner exit = new Scanner(System.in);
-            yesNo = exit.nextLine();
-        } while (yesNo.equalsIgnoreCase("y"));
+            answer = exit.nextLine();
+
+            while(yesNo) {
+                switch(answer) {
+                    case "y":
+                        yesNo = false;
+                        break;
+                    case "n":
+                        yesNo = false;
+                        break;
+                    default:
+                        yesNo = true;
+                        System.out.println("Введите [y/n]");
+                        answer = exit.nextLine();
+                }
+            }
+        } while (answer.equalsIgnoreCase("y"));
     }
 }
